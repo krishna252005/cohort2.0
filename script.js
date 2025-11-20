18f382232f1680a637027ffbb2c2a163b98cbf15
krishna <252005krishna@gmail.com>
2025-11-21 02:05:22 +0530

    updated bihar election

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
 
-var  boox = document.querySelector('#box')
+ var arr = [
+   { party:"BJP",
+   seats:89,
+   name:"Bartiya Janta Party",
+   logo:"BJP Logo Png _ Artworkbird.jpg"
+
+ },{ party:"JDU",
+   seats: 85,
+   logo:"images.jpg",
+   name:"Janata Dal (United)"
+
+ },{ party:"LJPRV",
+   seats: 19,
+  logo:"LJP_R_Logo.jpg",
+   name:"Lok Janshakti Party (Ram Vilas)"
+
+ },{party:"HAMS",
+   seats: 5,
+   logo:"1524615_hindustani_awam_morcha_logo.webp",
+   name:"Hindustani Awam Morcha"
+
+ },{party:"RJD",
+   seats: 25,
+   logo:"RJD_Logo.jpg",
+   name:"Rashtriya Janata Dal"
+
+ },{party:"INC",
+   seats: 6,
+   logo:"The Indian national congress was founded in December 1885_.jpg",
+   name:"Indian National Congress"
+},
+   
+   {party:"AIMIM",
+   seats: 5,
+   logo:"images (1).jpg",
+   name:"All India Majlis-e-Ittehadul Muslimeen"} , ];
+
+
+
+
+
+var  box = document.querySelector('#box')
 var clk = document.querySelector('button')
+var  party = document.querySelector('#party')
+var  seats = document.querySelector('#seats')
+var  form = document.querySelector('#form')
+var  logo = document.querySelector('#logo')
+var  body = document.querySelector('body')
 
 
 
 
 clk.addEventListener('click',function()
-{
-   var n1 = Math.floor(Math.random()*256)
-   var n2 = Math.floor(Math.random()*256)
-   var n3 = Math.floor(Math.random()*256)
-
-   boox.style.backgroundColor = `rgb(${n1},${n2},${n3})`
+{  console.log("hello");
+   var win =arr[Math.floor(Math.random()*arr.length)]
+   
+   party.innerHTML = win.party
+   seats.innerHTML = win.seats
+   form.innerHTML = win.name
+  
+  
     
 })
 
