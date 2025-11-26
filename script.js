4c6396c5dbfdf15b0b8860b1d88ac29fe7868a28
krishna <252005krishna@gmail.com>
2025-11-27 02:48:09 +0530

    spotlight effect

--- script.js
+++ script.js
@@ -1,35 +1,10 @@
 
-// addEventListener("mousemove",(e)=>{
-
-//  console.log(e.clientX,e.clientY)
-
-//  document.body.style.setProperty("--x",e.clientX +'px')
-//  document.body.style.setProperty("--y",e.clientY +'px')
-
-// })
-
-// ----------------------------------
-
-var para = document.querySelector('p')
-var characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
-var text = para.innerHTML
-
- para.addEventListener('mouseenter',() =>{
-
-
- setInterval(()=>{
-
-   
-    var str = text.split('').map(
-        (char,index) => {
-
-        return characters.split('')[Math.floor(Math.random()*53)]
-    }).join('')
-
-    para.innerHTML = str
-
- },100)
-
  
+ document.addEventListener("mousemove",(e)=>{
 
-})
+ console.log(e.clientX,e.clientY)
+
+ document.body.style.setProperty("--x",e.clientX +'px')
+ document.body.style.setProperty("--y",e.clientY +'px')
+
+ })
