60734d4b1170db92154c39eced6d28b6f672fec5
krishna <252005krishna@gmail.com>
2025-11-27 22:51:59 +0530

    matrix effext

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+var para = document.querySelector('p')
+var characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
+var text = para.innerHTML
+let interval = 0
+
+ para.addEventListener('mouseenter',() =>{
+
+    let iteration = 0
+    clearInterval(interval)
+
+  interval = setInterval(()=>{
+
+   
+    var str = text.split('').map((char,index) => {
+          if(index<iteration)
+          {
+            return char
+          }
+        return characters.split('')[Math.floor(Math.random()*52)]
+    }).join('')
+
+    para.innerHTML = str
+
+    iteration+=0.5
+
+
+ },50)
 
  
- document.addEventListener("mousemove",(e)=>{
 
- console.log(e.clientX,e.clientY)
-
- document.body.style.setProperty("--x",e.clientX +'px')
- document.body.style.setProperty("--y",e.clientY +'px')
-
- })
+})
